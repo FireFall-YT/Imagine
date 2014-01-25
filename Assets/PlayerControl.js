c882a9e2ec828c531b90783a6361a600001ca2aa
Heia- <ryan.gregory.cook@gmail.com>
2014-01-25 02:51:28 -0800

    Added controller support for one player. Also kept player support

--- Assets/PlayerControl.js
+++ Assets/PlayerControl.js
@@ -1,23 +1,27 @@
 ﻿#pragma strict
 
-var moveUp : KeyCode;
-var moveDown : KeyCode;
-var moveLeft : KeyCode;
-var moveRight : KeyCode;
-
-var speed = 10;
+var speed : float = 10.0;
 
 function Update () {
-	if(Input.GetKey(moveUp)){
-		rigidbody.velocity.z = speed;
-	}else if(Input.GetKey(moveDown)){
-		rigidbody.velocity.z = speed * -1;
-	}else if(Input.GetKey(moveRight)) {
-		rigidbody.velocity.x = speed;
-	}else if(Input.GetKey(moveLeft)) {
-		rigidbody.velocity.x = speed * -1;
-	}else {
-		rigidbody.velocity.x = 0;
-		rigidbody.velocity.z = 0;
-	}
+
+    var verticalMovement : float = Input.GetAxis ("Vertical") * speed;
+    var horizontalMovement : float = Input.GetAxis ("Horizontal") * speed;
+    
+    // Multiply by delta time
+    verticalMovement *= Time.deltaTime;
+    horizontalMovement *= Time.deltaTime;
+   
+    // Move the thing! 
+    transform.Translate(horizontalMovement, verticalMovement, 0);
+   
+	
+	 // A button
+    if (Input.GetKeyDown(KeyCode.Joystick1Button16) || Input.GetKeyDown(KeyCode.Joystick1Button0)) {
+    	gameObject.renderer.material.color = Color.green;
+    }
+    
+    // X button, 2 and 18
+    if (Input.GetKeyDown(KeyCode.Joystick1Button18) || Input.GetKeyDown(KeyCode.Joystick1Button2)) {
+    	gameObject.renderer.material.color = Color.yellow;
+    }
 }
