6ec17b91d603d86d2bc7ac113f68ecb8498b5f64
Taylor Owen <towenmil@ucsc.edu>
2014-01-26 11:36:10 -0800

    Added Lava

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -7,10 +7,16 @@ public class HealthScript : MonoBehaviour {
 	//Default value for the object being a player
 	public bool isPlayer = false;
 
+	// last stable position on a tile
+	Vector3 lastPos = new Vector3(0, 0, 0);
+
+	private int startHealth;
+
+	private int timer = 60;
 
 	// Use this for initialization
 	void Start () {
-	
+		startHealth = health;
 	}
 	
 	// Update is called once per frame
@@ -19,10 +25,32 @@ public class HealthScript : MonoBehaviour {
 			if(gameObject.CompareTag("Player")){
 				//revive player somewhere else
 
+				var playerTrans = gameObject.GetComponent<Transform>();
+
+				playerTrans.position = lastPos;
+				float zBuffer = playerTrans.position.z;
+
+				zBuffer += 3;
+
+				health = startHealth;
+
+
 			} else{
 				Destroy(gameObject);
 
 			}
+		}
+
+
+		if(timer > 0) timer--;
+		else timer = 60;
+
+	}
+
+	void onCollisionEnter(Collider collision){
+		if(collision.gameObject.tag == "Tile"/* && timer == 0*/){
+			var thisPos = gameObject.GetComponent<Transform>();
+			lastPos = thisPos.position;
 		}
 	}
 }
